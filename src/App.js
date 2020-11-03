a8f89bc2485a9dd7da78b6c1492d26443faef17f
Brett-migwi <brettmigwi75@gmail.com>
2020-11-03 11:18:57 +0300

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Nav from './components/Nav'
 
+import "bootstrap/dist/css/bootstrap.css"
+
+import "./style/css/main.css"
+
+import "jquery/dist/jquery.js"
+
+import "popper.js/dist/popper"
+
+import "bootstrap/dist/js/bootstrap"
+
+
 function App() {
   return (
     <div className="App">
        <Nav/>
+       <div className="myContainer bg-dark ">
+        
+       </div>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,80 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 
 export default function Nav() {
     return (
        <div>
-           <h2>This is our Nav Bar</h2>
+         <nav className="navbar navbar-expand-lg navbar-dark  fixed-top">
+         {/* Brand Name */}
+            <a  href ="#" className="navbar-brand">FandeMart</a>
+            <button className="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+               <span className="navbar-toggler-icon"></span>
+            </button>
+            {/* NAvigation Item List */}
+            <div id="my-nav" class="collapse navbar-collapse">
+               <ul class="navbar-nav mr-auto ml-auto">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Pages</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Features</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">shop</a>
+                  </li>
+               </ul>
+           </div>
+
+           {/* Navigation List Selector */}
+
+           <div className="container position-absolute nav-bucket-list d-none">
+                <div className="nav-bucket-list-inner">
+                     <div className="nav-bucket-item">
+                        <div className="head">Heading</div>
+                        <ul>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                        </ul>
+                     </div>
+                     <div className="nav-bucket-item">
+                     <div className="head">Heading</div>
+                        <ul>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                        </ul>
+                     </div>
+                     <div className="nav-bucket-item">
+                     <div className="head">Heading</div>
+                        <ul>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                        </ul>
+                     </div>
+                     <div className="nav-bucket-item">
+                     <div className="head">Heading</div>
+                        <ul>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                           <li>Product Item</li>
+                        </ul>
+                     </div>
+                </div>
+           </div>
+        </nav>
        </div>
     )
 }
